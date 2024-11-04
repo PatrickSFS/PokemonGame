360542a3f88d394793f4bbfdf73708dec895b540
PatrickSFS <patrick.freitas@engenharia.ufjf.br>
2024-11-04 18:48:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div >
+      <h1 className="text-4xl font-bold underline text-blue-500">
+        Hello Tailwind with Vite!
+      </h1>
+
+    </div>
+  );
+}
+
+export default App;
